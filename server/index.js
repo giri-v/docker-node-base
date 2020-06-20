f8599871c8f1d9fd9c04680585f9cbd70f1bb6b6
Giri Venkat <giri@venkat.com>
2020-06-19 23:08:05 -0700

    Added docker healthcheck logic to server

--- server/index.js
+++ server/index.js
@@ -125,6 +125,18 @@ app.route("/")
 app.use("/", express.static(angularDir), api);
 app.use("/api/v1", api); // To allow API version support
 
+
+/////////////////////////////////////
+// DOCKER HEALTHCHECK
+/////////////////////////////////////
+app.get('/healthz', function (req, res) {
+  // do app logic here to determine if app is truly healthy
+  // you should return 200 if healthy, and anything else will fail
+  // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
+  res.send('I am happy and healthy\n');
+});
+
+
 /////////////////////////////////////
 // ERROR HANDLING
 ////////////////////////////////////
